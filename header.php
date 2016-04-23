601a69c4ad48579abab630319db8f6f52b60160f
matheuswd <matheus@matheuswd.com>
2016-04-23 10:58:38 -0300

    fixing urls and started to separate the layout into PHP files

---
+++ header.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Luke">
+
+    <title>Writer</title>
+
+
+	<!--Fonts-->
+	<link href='http://fonts.googleapis.com/css?family=Oxygen:400,700,300&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Fira+Sans:400,300,500,700&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	
+    
+    <!--Zocial Icons-->
+	<link href='http://weloveiconfonts.com/api/?family=zocial' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/css/animate.css" rel="stylesheet">
+    <link href="<?php bloginfo( 'stylesheet_directory' ); ?>/style.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+   
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<div class="loader">
+  <img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/img/puff.svg" width="50" alt="loader">
+</div>
+
+
+    <!-- Page Content -->
+    <div class="container-fluid">
+    
+    <div class="row">
+    	
+    	
+    	<!--LEFT PART-->
+		<div class="col-sm-12 col-md-5 text-center fixed-left" style="background-image:url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/img/pt/1.jpeg);">
+		
+		
+		
+			<div class="logo site-title">
+				<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/img/sc1.png" alt="logo">
+			</div>
+		
+			<div class="show-menu">
+				<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/img/iconmenu.png" alt="menu">
+			</div>
+		
+		
+			<div class="bottom-wrapper">
+			
+			<h1>Reader awesome theme</h1>
+			<p>is a multi-author blog theme for the WordPress platform. Featuring a minimal, responsive, content-focused design and AJAX navigation. Available for purchase at ThemeForest.</p>
+			
+			<ul class="social-icons">
+			<li><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/img/facebook.png" alt="facebook" /></li>
+			<li><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/img/twitter.png" alt="twitter" /></li>
+			<li><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/img/pinterest.png" alt="pinterest" /></li>
+			</ul>
+			
+			</div>
+			
+			
+			<div class="overlayer">
+			
+			<div class="close-overlayer">
+				<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/img/iconclose.png" alt="close">
+			</div>
+			
+			<div class="overlayer-wrap animated fadeInLeft">
+			
+			<p>
+				Minions ipsum irure nisi sit amet duis potatoooo consequat voluptate tank yuuu! Bee do bee do bee do hana dul sae. Nisi nisi dolore ut irure aliqua adipisicing ut aliqua eiusmod. Pepete minim jeje voluptate. Aliqua bappleees ti aamoo! Bananaaaa hahaha jiji. Officia commodo ex et.
+<br/>
+
+			</p>
+			
+			
+			
+			<ul class="overlayer-menu">
+			<li>Home</li>
+			<li>About Me</li>
+			<li>Contact</li>
+			<li>Another Page</li>
+			</ul>
+			<p>
+			Minions ipsum irure nisi sit amet duis potatoooo consequat voluptate tank yuuu! Bee do bee do bee do hana dul sae. Nisi nisi dolore ut irure aliqua adipisicing ut aliqua eiusmod. Pepete minim jeje voluptate. Aliqua bappleees ti aamoo! Bananaaaa hahaha jiji. Officia commodo ex et.
+<br/>
+Hana dul sae quis aute exercitation qui aliqua tatata bala tu jiji consectetur. Consequat hahaha dolore aliqua eiusmod me want bananaaa! Bananaaaa. La bodaaa veniam tank yuuu! Duis. Butt quis uuuhhh voluptate aliqua ullamco bappleees. Elit hahaha ex sed ut bee do bee do bee do tatata bala tu nisi aliqua. Aliqua jeje jeje eiusmod hahaha jiji occaecat. Gelatooo jiji duis daa pepete baboiii velit. Officia bappleees ut tempor.
+<br/>
+Poulet tikka masala commodo poopayee velit. Poopayee bananaaaa underweaaar incididunt hahaha bee do bee do bee do po kass esse belloo! Wiiiii adipisicing gelatooo minim exercitation ut ut elit. Consectetur irure tatata bala tu qui consectetur irure dolor tank yuuu! Magna la bodaaa. Hahaha underweaaar incididunt aliqua adipisicing la bodaaa daa esse. Poopayee consectetur ullamco laboris hahaha elit ut ullamco. Aliquip exercitation daa eiusmod aliquip sit amet dolor eiusmod commodo sit amet.
+<br/>
+Quis tulaliloo daa ti aamoo! Consequat para tú nostrud potatoooo. Bee do bee do bee do aliqua jiji laboris belloo! Aliqua esse elit nostrud poopayee. Wiiiii sit amet tatata bala tu underweaaar poulet tikka masala potatoooo cillum. Et esse et magna. Me want bananaaa! magna duis ullamco elit incididunt po kass baboiii commodo magna tempor. La bodaaa dolor poopayee reprehenderit me want bananaaa! Tank yuuu! Aaaaaah bee do bee do bee do. Ad velit quis nostrud bee do bee do bee do bee do bee do bee do.
+<br/>			
+	Minions ipsum irure nisi sit amet duis potatoooo consequat voluptate tank yuuu! Bee do bee do bee do hana dul sae. Nisi nisi dolore ut irure aliqua adipisicing ut aliqua eiusmod. Pepete minim jeje voluptate. Aliqua bappleees ti aamoo! Bananaaaa hahaha jiji. Officia commodo ex et.
+<br/>
+Hana dul sae quis aute exercitation qui aliqua tatata bala tu jiji consectetur. Consequat hahaha dolore aliqua eiusmod me want bananaaa! Bananaaaa. La bodaaa veniam tank yuuu! Duis. Butt quis uuuhhh voluptate aliqua ullamco bappleees. Elit hahaha ex sed ut bee do bee do bee do tatata bala tu nisi aliqua. Aliqua jeje jeje eiusmod hahaha jiji occaecat. Gelatooo jiji duis daa pepete baboiii velit. Officia bappleees ut tempor.
+<br/>
+Poulet tikka masala commodo poopayee velit. Poopayee bananaaaa underweaaar incididunt hahaha bee do bee do bee do po kass esse belloo! Wiiiii adipisicing gelatooo minim exercitation ut ut elit. Consectetur irure tatata bala tu qui consectetur irure dolor tank yuuu! Magna la bodaaa. Hahaha underweaaar incididunt aliqua adipisicing la bodaaa daa esse. Poopayee consectetur ullamco laboris hahaha elit ut ullamco. Aliquip exercitation daa eiusmod aliquip sit amet dolor eiusmod commodo sit amet.
+<br/>
+Quis tulaliloo daa ti aamoo! Consequat para tú nostrud potatoooo. Bee do bee do bee do aliqua jiji laboris belloo! Aliqua esse elit nostrud poopayee. Wiiiii sit amet tatata bala tu underweaaar poulet tikka masala potatoooo cillum. Et esse et magna. Me want bananaaa! magna duis ullamco elit incididunt po kass baboiii commodo magna tempor. La bodaaa dolor poopayee reprehenderit me want bananaaa! Tank yuuu! Aaaaaah bee do bee do bee do. Ad velit quis nostrud bee do bee do bee do bee do bee do bee do.
+<br/>			
+			</p>
+			</div>
+			</div>
+		
+		</div>
+		
+		<!--LEFT PART-->
