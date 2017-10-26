6380846d276712fd9da0b3fe2b318b80c7a4fdf1
Matheus Martins <matheusdealmeidamartins@gmail.com>
2017-10-26 16:48:17 -0200

    Update header.php

--- header.php
+++ header.php
@@ -2,14 +2,13 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
     <meta name="description" content="">
     <link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<!-- Change title -->
     <title>Writer</title>
 
 
